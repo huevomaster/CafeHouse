25ed5ac71d1ea0b4e87a2940cad2acaebaca0e7e
Daniel Iglesias <daniel.iglemart@gmail.com>
2026-07-22 19:09:43 +0200

    Quitar precio y poner toda la carpeta

--- js/data.js
+++ js/data.js
@@ -6,8 +6,7 @@ export const menuItems = [
     descripcion: "Crema de café batida, leche fresca y canela.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/dalgona-coffee.jpg",
-    precio: 3.50
+    foto: "CafeHouse/images/dalgona-coffee.jpg",
   },
   {
     id: 2,
@@ -15,8 +14,7 @@ export const menuItems = [
     descripcion: "Doble espresso, leche condensada y leche fresca.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/spanish-iced-latte.jpg",
-    precio: 3.80
+    foto: "CafeHouse/images/spanish-iced-latte.jpg",
   },
   {
     id: 4,
@@ -24,8 +22,7 @@ export const menuItems = [
     descripcion: "Doble espresso, crema de oreo, leche fresca, galletas oreo, sirope de vainilla.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/oreo-cream-latte.png",
-    precio: 4.20
+    foto: "CafeHouse/images/oreo-cream-latte.png",
   },
   {
     id: 5,
@@ -33,8 +30,7 @@ export const menuItems = [
     descripcion: "Doble espresso, leche fresca, sirope de chocolate blanco, cold foam de oreo.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/oreo-white-mocha.png",
-    precio: 4.50
+    foto: "CafeHouse/images/oreo-white-mocha.png",
   },
   {
     id: 6,
@@ -42,8 +38,7 @@ export const menuItems = [
     descripcion: "Espresso suave, agua, sirope de vainilla y vanilla cold foam.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/salted-vanilla-iced-coffee.jpg",
-    precio: 3.90
+    foto: "CafeHouse/images/salted-vanilla-iced-coffee.jpg",
   },
   {
     id: 7,
@@ -51,8 +46,7 @@ export const menuItems = [
     descripcion: "Espresso suave, leche fresca, sirope de chocolate y caramelo.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/salted-caramel-iced-mocha.jpg",
-    precio: 4.20
+    foto: "CafeHouse/images/salted-caramel-iced-mocha.jpg",
   },
   {
     id: 8,
@@ -60,8 +54,7 @@ export const menuItems = [
     descripcion: "Doble espresso, leche fresca, canela, cacao en polvo y cold foam.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/cinnamon-mocha-iced-latte.png",
-    precio: 4.00
+    foto: "CafeHouse/images/cinnamon-mocha-iced-latte.png",
   },
   {
     id: 9,
@@ -69,8 +62,7 @@ export const menuItems = [
     descripcion: "Espresso suave, leche fresca y honey cream.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/cinnamon-honey-latte.png",
-    precio: 3.80
+    foto: "CafeHouse/images/cinnamon-honey-latte.png",
   },
   {
     id: 10,
@@ -78,8 +70,7 @@ export const menuItems = [
     descripcion: "Espresso suave, leche fresca y sirope de chocolate blanco.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/white-mocha-latte.png",
-    precio: 3.90
+    foto: "CafeHouse/images/white-mocha-latte.png",
   },
   {
     id: 11,
@@ -87,8 +78,7 @@ export const menuItems = [
     descripcion: "Crema de café con sirope de miel y leche fresca.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/honey-cream-coffee.png",
-    precio: 3.50
+    foto: "CafeHouse/images/honey-cream-coffee.png",
   },
   {
     id: 12,
@@ -96,8 +86,7 @@ export const menuItems = [
     descripcion: "Doble espresso, leche fresca, sirope de vainilla, y sirope de caramelo.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/iced-caramel-macchiato.png",
-    precio: 4.10
+    foto: "CafeHouse/images/iced-caramel-macchiato.png",
   },
   {
     id: 13,
@@ -105,8 +94,7 @@ export const menuItems = [
     descripcion: "Espresso suave, leche fresca, crema de cacahuete, y miel.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/peanut-butter-iced-latte.png",
-    precio: 4.30
+    foto: "CafeHouse/images/peanut-butter-iced-latte.png",
   },
   {
     id: 14,
@@ -114,8 +102,7 @@ export const menuItems = [
     descripcion: "Doble espresso, leche fresca, crema espumada de vainilla, y una fina capa caramelizada.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Clásicos",
-    foto: "images/vanilla-brulee-latte.png",
-    precio: 4.20
+    foto: "CafeHouse/images/vanilla-brulee-latte.png",
   },
 
   // CAFÉS FRÍOS - Exóticos
@@ -125,8 +112,7 @@ export const menuItems = [
     descripcion: "Doble espresso, leche fresca, fresas, caramelo y cold foam.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Exóticos",
-    foto: "images/strawberry-iced-latte.png",
-    precio: 4.50
+    foto: "CafeHouse/images/strawberry-iced-latte.png",
   },
   {
     id: 16,
@@ -134,8 +120,7 @@ export const menuItems = [
     descripcion: "Doble espresso, leche fresca, plátano, miel, canela y agua.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Exóticos",
-    foto: "images/banana-cream-iced-coffee.png",
-    precio: 4.50
+    foto: "CafeHouse/images/banana-cream-iced-coffee.png",
   },
   {
     id: 17,
@@ -143,8 +128,7 @@ export const menuItems = [
     descripcion: "Doble espresso y zumo de naranja.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Exóticos",
-    foto: "images/orange-coffee.png",
-    precio: 3.80
+    foto: "CafeHouse/images/orange-coffee.png",
   },
   {
     id: 18,
@@ -152,8 +136,7 @@ export const menuItems = [
     descripcion: "Espresso suave, agua tónica, y una tira de naranja.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Exóticos",
-    foto: "images/espresso-tonic.png",
-    precio: 3.50
+    foto: "CafeHouse/images/espresso-tonic.png",
   },
   {
     id: 19,
@@ -161,8 +144,7 @@ export const menuItems = [
     descripcion: "Doble espresso pasado por una rodaja de limon y un poco de sal y agua tónica.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Exóticos",
-    foto: "images/espresso-bomb.png",
-    precio: 3.20
+    foto: "CafeHouse/images/espresso-bomb.png",
   },
   {
     id: 20,
@@ -170,8 +152,7 @@ export const menuItems = [
     descripcion: "Crema de café y cacao en polvo.",
     categoria: "CAFÉS FRÍOS",
     subcategoria: "Exóticos",
-    foto: "images/crema-de-cafe-italiana.png",
-    precio: 3.00
+    foto: "CafeHouse/images/crema-de-cafe-italiana.png",
   },
 
   // CAFÉS CALIENTES
@@ -180,48 +161,42 @@ export const menuItems = [
     nombre: "Café Bombón",
     descripcion: "Espresso suave, leche condensada y un toque de leche espumada.",
     categoria: "CAFÉS CALIENTES",
-    foto: "images/cafe-bombon.png",
-    precio: 2.50
+    foto: "CafeHouse/images/cafe-bombon.png",
   },
   {
     id: 22,
     nombre: "Café con leche",
     descripcion: "Espresso suave y leche. (Hielo opcional)",
     categoria: "CAFÉS CALIENTES",
-    foto: "images/cappuccino.png",
-    precio: 2.20
+    foto: "CafeHouse/images/cappuccino.png",
   },
   {
     id: 23,
     nombre: "Americano",
     descripcion: "Espresso suave y agua caliente.",
     categoria: "CAFÉS CALIENTES",
-    foto: "images/americano.png",
-    precio: 2.00
+    foto: "CafeHouse/images/americano.png",
   },
   {
     id: 24,
     nombre: "Mocha",
     descripcion: "Café con chocolate (leche, blanco, negro), leche y espuma.",
     categoria: "CAFÉS CALIENTES",
-    foto: "images/mocha.png",
-    precio: 3.20
+    foto: "CafeHouse/images/mocha.png",
   },
   {
     id: 25,
     nombre: "Cappuccino",
     descripcion: "Espresso suave, leche, y abundante espuma.",
     categoria: "CAFÉS CALIENTES",
-    foto: "images/cappuccino.png",
-    precio: 2.80
+    foto: "CafeHouse/images/cappuccino.png",
   },
   {
     id: 26,
     nombre: "Café Marocchino",
     descripcion: "Espresso suave, leche espumada, crema de cacao y cacao en polvo.",
     categoria: "CAFÉS CALIENTES",
-    foto: "images/cafe marocchino.png",
-    precio: 3.00
+    foto: "CafeHouse/images/cafe marocchino.png",
   },
 
   // OTRAS BEBIDAS
@@ -230,24 +205,21 @@ export const menuItems = [
     nombre: "Matcha Latte",
     descripcion: "Matcha con leche caliente, leche fresca, miel, vainilla y caramelo.",
     categoria: "OTRAS BEBIDAS",
-    foto: "images/matcha-latte.png",
-    precio: 4.50
+    foto: "CafeHouse/images/matcha-latte.png",
   },
   {
     id: 28,
     nombre: "Frappé Especial",
     descripcion: "Frappé helado a elegir entre sabor Chocolate (negro/blanco/leche), kinder, Lotus o vainilla.",
     categoria: "OTRAS BEBIDAS",
-    foto: "images/frappe-especial.png",
-    precio: 4.80
+    foto: "CafeHouse/images/frappe-especial.png",
   },
   {
     id: 29,
     nombre: "Milkshakes",
     descripcion: "Batido cremoso con sabores a elegir: Vainilla, chocolate (negro/blanco/leche), oreo, caramelo, nocilla o lotus.",
     categoria: "OTRAS BEBIDAS",
-    foto: "images/milkshake.png",
-    precio: 4.50
+    foto: "CafeHouse/images/milkshake.png",
   },
 
   // APERITIVOS
@@ -256,39 +228,34 @@ export const menuItems = [
     nombre: "Sándwich Mixto",
     descripcion: "Queso fundido, jamón york de primera calidad y mantequilla dorada al grill.",
     categoria: "APERITIVOS",
-    foto: "images/sandwich-mixto.png",
-    precio: 3.50
+    foto: "CafeHouse/images/sandwich-mixto.png",
   },
   {
     id: 31,
     nombre: "Croissant Mixto",
     descripcion: "Croissant de mantequilla relleno de queso fundido y jamón york.",
     categoria: "APERITIVOS",
-    foto: "images/croissant-mixto.png",
-    precio: 3.20
+    foto: "CafeHouse/images/croissant-mixto.png",
   },
   {
     id: 32,
     nombre: "Tosta de Jamón y Tomate",
     descripcion: "Pan rústico tostado, jamón ibérico, tomate triturado (opcional) y aceite de oliva virgen extra.",
     categoria: "APERITIVOS",
-    foto: "images/tosta-de-jamon-y-tomate.png",
-    precio: 4.00
+    foto: "CafeHouse/images/tosta-de-jamon-y-tomate.png",
   },
   {
     id: 33,
     nombre: "Tabla Variada",
     descripcion: "Selección de quesos curados, fuet, lomo embuchado y almendras tostadas.",
     categoria: "APERITIVOS",
-    foto: "images/tabla-variada.png",
-    precio: 7.50
+    foto: "CafeHouse/images/tabla-variada.png",
   },
   {
     id: 34,
     nombre: "Bizcocho Casero",
     descripcion: "Delicioso bizcocho artesano del día, esponjoso y aromático.",
     categoria: "APERITIVOS",
-    foto: "images/bizcocho-casero.png",
-    precio: 2.80
+    foto: "CafeHouse/images/bizcocho-casero.png",
   }
 ];
